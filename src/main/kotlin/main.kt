e08d01393600f4dc156b45eed89e85ff8bfe6b3f
Luiz Moraes <lffm1994@gmail.com>
2021-05-31 13:23:51 -0300

    Módulo 2 -  Controle de fluxo e o paradigma procedural

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
-fun main(){
+fun main() {
 
     println("Olá Mundo. Bem vindo ao ByteBank")
 
@@ -18,28 +18,81 @@ fun main(){
     No kotlin ele é estaticamente tipado de forma implicita
      */
 
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo: Double  = 0.0
-    saldo = 100 + 2.0
-    //Mesmo resultado da soma
-    //saldo = saldo + 200
-    saldo += 200
+    var i = 0
+    while (i < 5) {
 
-    //Exibição simples
-    //print(titular)
-    //Exibição pulando linha
-    //println(titular)
-    //Forma de exibir String junto a variavel
-    //Concatenação
-    //println("titular " + titular)
-    //String Template
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = 0.0
 
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+
+        i++
+    }
+
+
+    //Estrutura de repetição
+    // for (i in 1..3) {
+    // for (i in 5 downTo 1) {
+
+//    for (i in 5 downTo 1) {
+//
+//        if(i == 4){
+//            continue
+//        }
+//        val titular: String = "Alex $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo: Double = 0.0
+//        saldo = i + 10.0
+//        Mesmo resultado da soma
+//        saldo = saldo + 200
+//        saldo += 200
+//        saldo -= 1000
+//
+//        //Exibição simples
+//        //print(titular)
+//        //Exibição pulando linha
+//        //println(titular)
+//        //Forma de exibir String junto a variavel
+//        //Concatenação
+//        //println("titular " + titular)
+//        //String Template
+//        println("titular $titular")
+//        println("número da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//        //Controle de fluxo - expressão booleana
+//        //testaCondicoes(saldo)
+//
+//
+//    }
 
 
 }
 
+//Função precisa informar variavel + tipo
+fun testaCondicoes(saldo: Double) {
 
+    if (saldo > 0.0) {
+        println("Conta é positiva")
+    } else if (saldo == 0.0) {
+        println("Conta é neutra")
+    } else {
+        println("Conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> {
+            println("Conta é positiva")
+        }
+        saldo == 0.0 -> {
+            println("Conta é neutra")
+        }
+        else -> {
+            println("Conta é negativa")
+        }
+    }
+}
