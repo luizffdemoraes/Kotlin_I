e997499924c9d9da7168901c8d8315195323d366
Luiz Moraes <lffm1994@gmail.com>
2021-06-01 03:07:04 -0300

    Módulo 3 e 4 - Conhecendo a orientação a objeto e implamentando comportamentos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,145 @@
 fun main() {
+    println("Bem vindo ao ByteBank.")
+    //Instancia
+    val contaAlex = Conta()
+    contaAlex.setTitular("Alex")
+    contaAlex.setNumero(1000)
+    contaAlex.setSaldo(-200.0)
 
-    println("Olá Mundo. Bem vindo ao ByteBank")
+    var contaFran = Conta()
+    contaFran.setTitular("Fran")
+    contaFran.setNumero(1001)
+    contaFran.setSaldo(300.0)
 
-    /*
-    Variaveis
-    -> var - seu valor pode ser modificado - mutavel
-    -> val - seu valor não pode ser modificado - imutavel
-    Não precisamos indicar o tipo ao inserir valor o tipo de valor fica implicito
-    print não pula linha e necessario usar o println
+    println(contaAlex.getTitular())
+    println(contaAlex.getNumero())
+    println(contaAlex.getSaldo())
 
-    Tecnica String template inserir variaveis no escopo de uma string
+    println(contaFran.getTitular())
+    println(contaFran.getNumero())
+    println(contaFran.getSaldo())
 
-    Restrição
-    -> Ao criar uma variavel é necessário inicialização/atribuir um valor
+    println("depositando na conta do Fran")
+    //Classe se torna responsavel em alterar seus atributos
+    contaAlex.deposita(50.0)
+    println(contaAlex.getSaldo())
 
-    Declarar o tipo da variavel com : String
-    No kotlin ele é estaticamente tipado de forma implicita
-     */
+    println("depositando na conta do Alex")
+    contaFran.deposita(70.0)
+    println(contaFran.getSaldo())
 
+    println("sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.getSaldo())
+
+    println("sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.getSaldo())
+
+    println("saque em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.getSaldo())
+
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.getSaldo())
+
+    println("Transferência da conta da Fran para o Alex")
+
+    if (contaFran.transfere(100.0, contaAlex)) {
+        println("Tranferência bem sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+
+    println(contaAlex.getSaldo())
+    println(contaFran.getSaldo())
+}
+
+class Conta {
+    private var titular = ""
+    private var numero = 0
+    private var saldo = 0.0
+
+    //função menbro da classe - Metodos ou Comportamentos da classe
+    fun deposita(valor: Double) {
+        saldo += valor
+    }
+
+    fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            saldo -= valor
+        }
+
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.deposita(valor)
+            return true
+        }
+        return false
+    }
+
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(valor: Double) {
+        if (valor > 0) {
+            saldo = valor
+        }
+    }
+
+    fun getTitular(): String {
+        return titular
+    }
+
+    fun setTitular(titular: String) {
+        this.titular = titular
+    }
+
+    fun getNumero(): Int {
+        return numero
+    }
+
+    fun setNumero(numero: Int) {
+        this.numero = numero
+
+    }
+
+
+}
+
+fun testaCopiaEReferencias() {
+
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.setTitular("João")
+    //Atribuição de referenfia
+    //var contaMaria = contaJoao
+    var contaMaria = Conta()
+    contaMaria.setTitular("Maria")
+    //contaJoao.titular = "João"
+
+    println("Titular conta João: ${contaJoao.getTitular()}")
+    println("Titular conta Maria: ${contaMaria.getTitular()}")
+
+    println(contaJoao)
+    println(contaMaria)
+
+}
+
+//Controler de Estrutura de repetição
+fun testaLacos() {
+    //Estrutura de repetição while
     var i = 0
     while (i < 5) {
 
@@ -33,46 +155,37 @@ fun main() {
         i++
     }
 
-
-    //Estrutura de repetição
-    // for (i in 1..3) {
-    // for (i in 5 downTo 1) {
-
+    //Estrutura de repetição for
+//    for (i in 1..3) {
 //    for (i in 5 downTo 1) {
-//
-//        if(i == 4){
-//            continue
-//        }
-//        val titular: String = "Alex $i"
-//        val numeroConta: Int = 1000 + i
-//        var saldo: Double = 0.0
-//        saldo = i + 10.0
-//        Mesmo resultado da soma
-//        saldo = saldo + 200
-//        saldo += 200
-//        saldo -= 1000
-//
-//        //Exibição simples
-//        //print(titular)
-//        //Exibição pulando linha
-//        //println(titular)
-//        //Forma de exibir String junto a variavel
-//        //Concatenação
-//        //println("titular " + titular)
-//        //String Template
-//        println("titular $titular")
-//        println("número da conta $numeroConta")
-//        println("saldo da conta $saldo")
-//        println()
-//        //Controle de fluxo - expressão booleana
+
+    for (i in 5 downTo 1) {
+
+        if (i == 4) {
+            continue
+        }
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = 0.0
+        saldo = i + 10.0
+//      Mesmo resultado da soma
+        saldo = saldo + 200
+        saldo += 200
+        saldo -= 1000
+
+
+        //String Template
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+
 //        //testaCondicoes(saldo)
-//
-//
-//    }
 
-
+    }
 }
 
+//Controle de fluxo - expressão booleana
 //Função precisa informar variavel + tipo
 fun testaCondicoes(saldo: Double) {
 
@@ -96,3 +209,28 @@ fun testaCondicoes(saldo: Double) {
         }
     }
 }
+
+/*
+    Variaveis
+    -> var - seu valor pode ser modificado - mutavel
+    -> val - seu valor não pode ser modificado - imutavel
+    Não precisamos indicar o tipo ao inserir valor o tipo de valor fica implicito
+    print não pula linha e necessario usar o println
+
+    Tecnica String template inserir variaveis no escopo de uma string
+
+    Restrição
+    -> Ao criar uma variavel é necessário inicialização/atribuir um valor
+
+    Declarar o tipo da variavel com : String
+    No kotlin ele é estaticamente tipado de forma implicita
+
+    Exibição simples
+    print(titular)
+    Exibição pulando linha
+    println(titular)
+    Forma de exibir String junto a variavel
+    Concatenação
+    println("titular " + titular)
+
+*/
